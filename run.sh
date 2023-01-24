4ba4196d21a5587dfc95a8211d63f96c91d3bce6
rsotnas <43410397+rsotnas@users.noreply.github.com>
2023-01-24 11:33:08 -0300

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-export MONGO_DB="vinfast_dev"
-export MONGO_URL="mongodb://studies:62e05dd021ccc531bd645b2e@143.244.167.128:27017/?authSource=admin&readPreference=primary&ssl=false"
-export USER_DEFAULT_PASSWORD="vinfast"
-export JWT_PRIVATE_KEY_FILE="./keys/jwtRS256.key"
-export JWT_PUBLIC_KEY_FILE="./keys/jwtRS256.key.pub"
-export PORT="6030"
-export STATION='Test'
-
-npm run watch:dev
