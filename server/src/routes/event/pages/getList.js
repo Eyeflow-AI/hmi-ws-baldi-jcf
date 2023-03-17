db32408ec3ffce4e3b73d20527740a8e5bc9634c
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-03-17 16:59:07 -0300

    Get List working

--- server/src/routes/event/pages/getList.js
+++ server/src/routes/event/pages/getList.js
@@ -1,5 +1,6 @@
 import Mongo from "../../../components/mongo";
 import isIsoDate from "../../../utils/isIsoDate";
+import hashCode from "../../../utils/hashCode";
 
 function raiseError(message) {
   let err = new Error(message);
@@ -10,8 +11,6 @@ function raiseError(message) {
 function getMatch(reqQuery) {
 
   let match = {};
-  let err = new Error();
-  err.status = 400;
 
   if (Boolean(reqQuery) && Object.keys(reqQuery).length > 0) {
 
@@ -38,7 +37,7 @@ async function getList(req, res, next) {
     let match = getMatch(req.query);
     let projection = {
       _id: true,
-      id: { $substr: [ "$event_data.car_data.vin", 11, -1 ] },
+      id: { $substr: [ "$event_data.car_data.vin", 11, -1 ] }, //TODO: Get from config file
       event_time: true,
       status: {
         "$cond": [
@@ -46,17 +45,30 @@ async function getList(req, res, next) {
           "ok", 
           "nok"
         ]
-      },                                      //TODO implement repaired and unidentified logic
-      thumbURL: "/assets/ItemButtonImage.svg" //TODO: Get from config file
+      },                                                       //TODO implement repaired and unidentified logic
+      thumbURL: "/assets/ItemButtonImage.svg"                  //TODO: Get from config file
     };
-    let collection = "inspection_events";     //TODO: Get from config file
-    let limit = 10000;                        //TODO: Get from config file
-    let eventList = await Mongo.db.collection(collection).find(match, {projection}).limit(limit).toArray();
+    let collection = "inspection_events";                      //TODO: Get from config file
+    let limit = 10000;                                         //TODO: Get from config file
 
-    let output = {ok: true, eventListLength: eventList.length, eventList};
+    let dateString;
+    let eventList = await Mongo.db.collection(collection).find(match, {projection}).limit(limit).toArray();
+    let eventListLength = eventList.length;
+    eventList.forEach((el, index) => {
+      dateString += el.event_time.toISOString();
+      el.index = eventListLength - index;
+    });
+
+    let output = {
+      ok: true,
+      eventListLength,
+      eventList,
+      hash: hashCode(dateString)
+    };
     if (process.env.NODE_ENV === "development") {
       output.queryOptions = {match, projection, collection, limit};
     };
+
     res.status(200).json(output);
   }
   catch (err) {
---
+++ server/src/utils/hashCode.js
@@ -0,0 +1,3 @@
+export default function hashCode (s) {
+  return s.split('').reduce((a,b)=>{a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+};
