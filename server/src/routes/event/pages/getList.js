e31b01372ab9de024d7ad7222b866051d009a1de
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-03-17 15:45:42 -0300

    Check query working

--- server/src/routes/event/pages/getList.js
+++ server/src/routes/event/pages/getList.js
@@ -1,20 +1,33 @@
 import Mongo from "../../../components/mongo";
+import isIsoDate from "../../../utils/isIsoDate";
 
+function raiseError(message) {
+  let err = new Error(message);
+  err.status = 400;
+  throw err;
+};
 
 function getMatch(reqQuery) {
 
   let match = {};
+  let err = new Error();
+  err.status = 400;
+
   if (Boolean(reqQuery) && Object.keys(reqQuery).length > 0) {
+
     if (reqQuery.hasOwnProperty("min_event_time") || reqQuery.hasOwnProperty("max_event_time")) {
       match.event_time = {};
       if (reqQuery.hasOwnProperty("min_event_time")) {
+        if (!isIsoDate(reqQuery["min_event_time"])) {raiseError("Invalid min_event_time . Valid iso date is required.")};
         match.event_time["$gte"] = new Date(reqQuery.min_event_time);
       }
       if (reqQuery.hasOwnProperty("max_event_time")) {
-        match.event_time["$lte"] = new Date(reqQuery.min_event_time);
-      }
-    }
-  }
+        if (!isIsoDate(reqQuery["max_event_time"])) {raiseError("Invalid max_event_time . Valid iso date is required.")};
+        match.event_time["$lte"] = new Date(reqQuery.max_event_time);
+      };
+    };
+
+  };
 
   return match;
 };
