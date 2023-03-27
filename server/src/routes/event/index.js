13ff354ae7373395f45bc72d3b5b37b51a2dd7f5
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-03-27 15:59:00 -0300

    Removed post event from hmi-ws

--- server/src/routes/event/index.js
+++ server/src/routes/event/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {
   GetList,
   Get,
-  Post
 } from './pages';
 
 const router = express.Router();
@@ -10,6 +9,5 @@ const router = express.Router();
 
 router.get('/list', GetList);
 router.get('/:eventId', Get);
-router.get('/', Post);
 
 export default router;
