ed488fbf787418b9b2b21f0e4d449d63e192f36f
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-03-29 17:45:58 -0300

    added components to updateConfig

--- server/src/routes/batch/pages/get.js
+++ server/src/routes/batch/pages/get.js
@@ -7,19 +7,7 @@ async function get(req, res, next) {
     let batchId = req.params.batchId;
     let result = await Mongo.db.collection("batch").findOne({_id: Mongo.ObjectId(batchId)});
     if (result) {
-      //TODO get count data
-      let countData = {
-        status: [
-          {label: "OK", value: Math.floor(Math.random() * 500)},
-          {label: "NOK", value: Math.floor(Math.random() * 500)},
-        ],
-        topAnomalies: [
-          {label: "FOO", value: Math.floor(Math.random() * 500)},
-          {label: "BAR", value: Math.floor(Math.random() * 500)},
-        ]
-      };
-      countData.topAnomalies.sort((a,b) => b.value - a.value);
-      res.status(200).json({ok: true, batch: result, countData});
+      res.status(200).json({ok: true, batch: result});
     }
     else {
       let err = new Error(`Batch with _id ${batchId} does not exist`);
