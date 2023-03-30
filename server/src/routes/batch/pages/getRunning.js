07df927ce698e4cd0aed240fe32d7eabaa3ce887
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-03-30 11:31:19 -0300

    Chanes in Get Running Batch projection

--- server/src/routes/batch/pages/getRunning.js
+++ server/src/routes/batch/pages/getRunning.js
@@ -4,12 +4,21 @@ async function getRunning(req, res, next) {
 
   try {
     let stationId = req.params.stationId;
+    let projection = {
+      _id: true,
+      id: true,
+      label: true,
+      start_time: true,
+      end_time: true,
+      station: true,
+      status: true,
+    };
     if (!Mongo.ObjectId.isValid(stationId)) {
       let err = new Error(`${stationId} is not a valid station id`);
       err.status = 400;
       throw err;
     };
-    let result = await Mongo.db.collection("batch").findOne({station: Mongo.ObjectId(stationId), status: "running"})
+    let result = await Mongo.db.collection("batch").findOne({station: Mongo.ObjectId(stationId), status: "running"}, {projection});
     res.status(200).json({ok: true, batch: result ?? null});
   }
   catch (err) {
