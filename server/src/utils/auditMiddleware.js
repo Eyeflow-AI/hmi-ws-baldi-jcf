516458bfff1d5664ea1dc21e4e0c4cf7815e5f29
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-03-31 17:15:39 -0300

    Audit middleware inserting on mongo

--- server/src/utils/auditMiddleware.js
+++ server/src/utils/auditMiddleware.js
@@ -1,17 +1,35 @@
 import log from './log';
+import Mongo from '../components/mongo';
 
 const auditMiddleware = () => (req, res, next) => {
-  const method = req?.method?.toLowerCase();
-  const url = req?.url;
-  const requestId = req?.app?.requestId;
-  const body = { ...req?.body } ?? {};
-  const token = { ...req?.app?.auth } ?? {};
 
-  delete body.password;
-  delete token.password;
-  if (['post', 'put', 'patch', 'delete'].includes(method)) {
-    log.audit(`${method.toUpperCase()} - RequestId: ${requestId}. URL: ${url}. Body: ${JSON.stringify(body)} . Auth Info: ${JSON.stringify(token)}`);
-  };
+  res.on('finish', () => {
+    const method = req?.method?.toLowerCase();
+    const url = req?.url;
+    const requestId = req?.app?.requestId;
+    const body = { ...req?.body } ?? {};
+    const tokenData = { ...req?.app?.auth } ?? {};
+
+    if (body.password) {
+      body.password = "******";
+    };
+    if (tokenData.password) {
+      tokenData.password = "******";
+    };
+
+    if (['post', 'put', 'patch', 'delete'].includes(method)) {
+      Mongo.db.collection("audit").insertOne({
+        request_id: requestId,
+        status: res.statusCode < 400 ? "OK" : "NG",
+        method,
+        url,
+        body,
+        token_data: tokenData,
+      })
+        .then(()=>null)
+        .catch(() => log.audit(`Failed to insert audit. ${method.toUpperCase()} - RequestId: ${requestId}. Status: ${res.statusCode < 400 ? "OK" : "NG"}. URL: ${url}. Body: ${JSON.stringify(body)} . Auth Info: ${JSON.stringify(token)}`))
+    }; //TODO: Send to Sergio?
+ });
   next();
 };
 
