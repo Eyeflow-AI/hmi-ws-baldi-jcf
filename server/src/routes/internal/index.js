a5fa6f57e1d5f2d72e032f62bea6cf45aa7b482c
Felipe Alves <fas.felipesantos@gmail.com>
2023-04-14 16:51:05 -0300

    Get Languages Data Request

--- server/src/routes/internal/index.js
+++ server/src/routes/internal/index.js
@@ -4,12 +4,14 @@ import {
   GetIconInfo,
   PutActiveDataset,
   GetFromToDocument,
+  GetLanguagesData,
 } from './pages';
 
 const router = express.Router();
 
 router.get('/from-to-document', GetFromToDocument);
 router.get('/package-data', GetPackageData);
+router.get('/languages-data', GetLanguagesData);
 router.get('/icon-info/:icon', GetIconInfo);
 router.put('/active-dataset', PutActiveDataset);
 
