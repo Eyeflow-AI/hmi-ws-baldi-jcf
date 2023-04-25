a657018eb3ea34f68bf04f50a48f9cb96865eebc
Felipe Alves <fas.felipesantos@gmail.com>
2023-04-24 22:04:32 -0300

    queryBuilder more generic

--- server/src/utils/queryBuilder.js
+++ server/src/utils/queryBuilder.js
@@ -1,19 +1,30 @@
 import { BSON, EJSON, ObjectId } from 'bson';
 
-function variablesReplacer({ obj, variables }) {
+const _replaceAll = function (text, search, replacement) {
+  let newText = text;
+  return newText.split(search).join(replacement);
+};
 
+
+function functionEvaluator({ value, func, variableName }) {
+  let prepareFunc = _replaceAll(func, `{{${variableName}}}`, value);
+  let result = eval(prepareFunc);
+  return result;
+}
+
+function variablesReplacer({ obj, variables, variablesInfo }) {
   try {
     if (Array.isArray(obj)) {
       for (let i = 0; i < obj.length; i++) {
         if (obj[i] !== null && typeof obj[i] === 'object') {
-          obj[i] = variablesReplacer({ obj: obj[i], variables });
+          obj[i] = variablesReplacer({ obj: obj[i], variables, variablesInfo });
         }
       };
     }
     else {
       for (let [key, value] of Object.entries(obj)) {
         if (value !== null && typeof value === 'object') {
-          variablesReplacer({ obj: value, variables });
+          variablesReplacer({ obj: value, variables, variablesInfo });
         }
         else if (typeof (value) === 'string') {
           if (value.includes("{{")) {
@@ -22,7 +33,15 @@ function variablesReplacer({ obj, variables }) {
             if (replaceStr) {
               let variableName = replaceStr.split('{{')[1].replace('}}}', '');
               let _key = replaceStr.split(' ')[0].replace('{', '').replace(':', '');
-              let _value = replaceStr.split(' ')[1].replace(`{{${variableName}}`, variables[variableName]).replace('}}', '');
+              let resultCalculation = variables?.[variableName] ?? '';
+              if (variablesInfo?.[variableName]?.function) {
+                resultCalculation = functionEvaluator({
+                  value: variables?.[variableName],
+                  func: variablesInfo?.[variableName]?.function,
+                  variableName,
+                })
+              }
+              let _value = replaceStr.split(' ')[1].replace(`{{${variableName}}`, resultCalculation).replace('}}', '');
               obj[key] = EJSON.parse(JSON.stringify({ [_key]: _value }));
             }
           }
@@ -41,6 +60,6 @@ export default function queryBuilder({
   query,
   variables,
 }) {
-  let queryOBJ = variablesReplacer({ obj: query?.pipeline, variables });
+  let queryOBJ = variablesReplacer({ obj: query?.pipeline, variables, variablesInfo: query?.variables });
   return queryOBJ;
 }
