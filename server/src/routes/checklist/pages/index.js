d07fbff928cedeb53a2f81e9568bbff9278f9d36
Felipe Alves <fas.felipesantos@gmail.com>
2023-06-06 14:49:30 -0300

    adjstument in checklist

--- server/src/routes/checklist/pages/index.js
+++ server/src/routes/checklist/pages/index.js
@@ -3,3 +3,4 @@ export { default as GetChecklistRecipe } from './getChecklistRecipe';
 export { default as GetChecklistRegions } from './getChecklistRegions';
 export { default as GetChecklistSchemas } from './getChecklistSchemas';
 export { default as PutChecklistReference } from './putChecklistReference';
+export { default as PutReferenceToSchema } from './putReferenceToSchema';
