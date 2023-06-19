80b3ebc66a65002b6fbcc4b62ec3093c25618e5b
Felipe Alves <fas.felipesantos@gmail.com>
2023-06-19 17:49:29 -0300

    .

--- server/src/routes/station/index.js
+++ server/src/routes/station/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import {
   GetList,
+  Put,
 } from './pages';
+import { isAuthenticated, isAuthorized } from '../auth';
 
 const router = express.Router();
 
 
 router.get('/list', GetList);
+router.put('/:stationId', isAuthenticated, isAuthorized(['master']), Put);
 
 
 export default router;
--- server/src/routes/station/pages/index.js
+++ server/src/routes/station/pages/index.js
@@ -1,1 +1,2 @@
 export { default as GetList } from './getList';
+export { default as Put } from './put';
