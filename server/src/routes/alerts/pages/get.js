b4d0de03e043a06f23313958ad7930b8ad80a69f
Gabriel Melo <gabrielmelo1992@gmail.com>
2023-06-21 16:38:15 -0300

    alerts

--- server/src/routes/alerts/pages/get.js
+++ server/src/routes/alerts/pages/get.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 import Mongo from "../../../components/mongo";
 
 
@@ -5,7 +6,12 @@ async function get(req, res, next) {
   try {
     let station_id = req.params.stationId;
     let alerts = await Mongo.db.collection('alert').find({ station_id: Mongo.ObjectId(station_id), active: true }).toArray();
-    res.status(200).json({ok: true, alerts});
+    let alertsHash = "";
+    alerts.forEach(alert => {
+      alertsHash += alert._id.toString() + alert.date.toString();
+    });
+    alertsHash = crypto.createHash('md5').update(alertsHash).digest('hex');
+    res.status(200).json({ok: true, alerts, alertsHash});
   }
   catch (err) {
     next(err);
