185078de92a4c69212df88c1d5ec83d1ce2ea3a5
Adriane Santos <adriane.santos@siliconlife.ai>
2023-07-14 14:53:42 -0300

    Update get.js

--- server/src/routes/alerts/pages/get.js
+++ server/src/routes/alerts/pages/get.js
@@ -1,21 +1,22 @@
 const crypto = require('crypto');
 import Mongo from "../../../components/mongo";
 
-
 async function get(req, res, next) {
   try {
     let station_id = req.params.stationId;
     let alerts = await Mongo.db.collection('alert').find({ station_id: new Mongo.ObjectId(station_id), active: true }).toArray();
     let alertsHash = "";
+    let alertId = "";
     alerts.forEach(alert => {
       alertsHash += alert._id.toString() + alert.date.toString();
+      alertId = alert._id.toString();
     });
     alertsHash = crypto.createHash('md5').update(alertsHash).digest('hex');
-    res.status(200).json({ok: true, alerts, alertsHash});
+    res.status(200).json({ ok: true, alerts, alertId, alertsHash });
   }
   catch (err) {
     next(err);
   }
-};
+}
 
 export default get;
