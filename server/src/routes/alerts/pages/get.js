0a58ed2e74791398665aedcb542a2e3b52c41ef9
Adriane Santos <59652237+AdrianeRock@users.noreply.github.com>
2023-07-17 12:14:53 -0300

    Update get.js

--- server/src/routes/alerts/pages/get.js
+++ server/src/routes/alerts/pages/get.js
@@ -6,13 +6,11 @@ async function get(req, res, next) {
     let station_id = req.params.stationId;
     let alerts = await Mongo.db.collection('alert').find({ station_id: new Mongo.ObjectId(station_id), active: true }).toArray();
     let alertsHash = "";
-    let alertId = "";
     alerts.forEach(alert => {
       alertsHash += alert._id.toString() + alert.date.toString();
-      alertId = alert._id.toString();
     });
     alertsHash = crypto.createHash('md5').update(alertsHash).digest('hex');
-    res.status(200).json({ ok: true, alerts, alertId, alertsHash });
+    res.status(200).json({ ok: true, alerts, alertsHash });
   }
   catch (err) {
     next(err);
