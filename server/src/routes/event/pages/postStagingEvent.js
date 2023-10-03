d13fa46929a2716c09d057ed5c0f254776f2c3d1
Felipe Alves <fas.felipesantos@gmail.com>
2023-10-03 17:13:42 -0300

    adjusts to run locally

--- server/src/routes/event/pages/postStagingEvent.js
+++ server/src/routes/event/pages/postStagingEvent.js
@@ -5,12 +5,29 @@ async function postInspectionEvent(req, res, next) {
 
   try {
     //TODO
+    let ipv4Address;
+    const ipv4Pattern = /::ffff:(\d+\.\d+\.\d+\.\d+)/;
+    const clientIP = req.connection.remoteAddress;
+    const match = clientIP.match(ipv4Pattern);
+
     let event = req.body;
     event = JSON.stringify(event);
     event = EJSON.parse(event);
+    if (event?.event_host) {
+      event.host = event.event_host;
+      ipv4Address = event.event_host;
+      delete event.event_host;
+    }
+    else {
+      event.host = clientIP;
+      if (match && match.length >= 2) {
+        ipv4Address = match[1];
+        console.log("IPv4 address:", ipv4Address);
+      } else {
+        console.log("No IPv4 address found.");
+      }
+    }
 
-    const clientIP = req.connection.remoteAddress;
-    event.host = clientIP;
 
     await Mongo.db.collection('staging_events').insertOne(event);
     res.status(201).json({
