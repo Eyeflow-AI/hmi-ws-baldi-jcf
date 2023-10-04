f12c2caa6a298baebccc20214b8882283728d85a
Felipe Alves <fas.felipesantos@gmail.com>
2023-10-04 18:19:48 -0300

    tasks adjuted

--- server/src/routes/tasks/pages/get.js
+++ server/src/routes/tasks/pages/get.js
@@ -1,13 +1,21 @@
 import Mongo from "../../../components/mongo";
+import { BSON, EJSON, ObjectId } from 'bson';
+
 
 async function get(req, res, next) {
   try {
-    let edgeId = req.params.edgeId;
+    // let edgeId = req.params.edgeId;
     let status = req.query.status ?? "created";
-    let mongoFind = { edge_id: new Mongo.ObjectId(edgeId), active: true };
+    let query = req.query.query ?? "";
+    // console.log({query})
+    query = EJSON.parse(query);
+    let mongoFind = { active: true };
     if (!["all", ""].includes(status)) mongoFind.status = status;
-
+    if (query) mongoFind = { ...mongoFind, ...query };
+    // console.dir({ mongoFind }, {depth: null})
     let tasks = await Mongo.db.collection('tasks').find(mongoFind).toArray();
+    tasks = EJSON.stringify(tasks);
+    tasks = JSON.parse(tasks);
     res.status(200).json({ ok: true, tasks });
   }
   catch (err) {
