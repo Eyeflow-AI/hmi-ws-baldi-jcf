581304d2dfb51f29aff349102134b9a26b30d2d7
Felipe <fas.felipesantos@gmail.com>
2024-02-08 17:24:43 -0300

    added part_id

--- server/src/routes/batch/pages/getList.js
+++ server/src/routes/batch/pages/getList.js
@@ -7,25 +7,30 @@ function raiseError(message) {
   let err = new Error(message);
   err.status = 400;
   throw err;
-};
+}
 
 function getMatch(reqParams, reqQuery) {
-
   let match = {};
 
   if (Boolean(reqQuery) && Object.keys(reqQuery).length > 0) {
-
-    if (reqQuery.hasOwnProperty("min_event_time") || reqQuery.hasOwnProperty("max_event_time")) {
+    if (
+      reqQuery.hasOwnProperty("min_event_time") ||
+      reqQuery.hasOwnProperty("max_event_time")
+    ) {
       match.start_time = {};
       if (reqQuery.hasOwnProperty("min_event_time")) {
-        if (!isIsoDate(reqQuery["min_event_time"])) { raiseError("Invalid min_event_time. Valid iso date is required.") };
+        if (!isIsoDate(reqQuery["min_event_time"])) {
+          raiseError("Invalid min_event_time. Valid iso date is required.");
+        }
         match.start_time["$gte"] = new Date(reqQuery.min_event_time);
       }
       if (reqQuery.hasOwnProperty("max_event_time")) {
-        if (!isIsoDate(reqQuery["max_event_time"])) { raiseError("Invalid max_event_time. Valid iso date is required.") };
+        if (!isIsoDate(reqQuery["max_event_time"])) {
+          raiseError("Invalid max_event_time. Valid iso date is required.");
+        }
         match.start_time["$lte"] = new Date(reqQuery.max_event_time);
-      };
-    };
+      }
+    }
 
     for (let key of Object.keys(reqQuery)) {
       if (key.startsWith("info.")) {
@@ -33,27 +38,31 @@ function getMatch(reqParams, reqQuery) {
       }
     }
 
-    if (!Mongo.ObjectId.isValid(reqParams["stationId"])) { raiseError(`Invalid station ${reqParams["stationId"]}. Valid ObjectId is required.`) }
+    if (!Mongo.ObjectId.isValid(reqParams["stationId"])) {
+      raiseError(
+        `Invalid station ${reqParams["stationId"]}. Valid ObjectId is required.`
+      );
+    }
 
     match.station = new Mongo.ObjectId(reqParams["stationId"]);
-  };
+  }
 
   return match;
-};
+}
 
 /**
-* @param {string} [query.min_event_time] - min event time - Iso String Date.
-* @param {string} [query.max_event_time] - max event time - Iso String Date.
-* @param {string} [query.station] - station - String ObjectId.
-*/
+ * @param {string} [query.min_event_time] - min event time - Iso String Date.
+ * @param {string} [query.max_event_time] - max event time - Iso String Date.
+ * @param {string} [query.station] - station - String ObjectId.
+ */
 async function getList(req, res, next) {
-
   try {
     let match = getMatch(req.params, req.query);
     let projection = {
       _id: true,
       id: true,
       label: "$info.production_order",
+      part_id: "$info.part_id",
       start_time: true,
       end_time: true,
       station: true,
@@ -61,10 +70,15 @@ async function getList(req, res, next) {
     };
 
     let collection = "batch";
-    let limit = 10000;                                          //TODO: Get from config file
+    let limit = 10000; //TODO: Get from config file
     let sort = { start_time: -1 };
     let hashString;
-    let batchList = await Mongo.db.collection(collection).find(match, { projection }).sort(sort).limit(limit).toArray();
+    let batchList = await Mongo.db
+      .collection(collection)
+      .find(match, { projection })
+      .sort(sort)
+      .limit(limit)
+      .toArray();
 
     let batchListLength = batchList.length;
     batchList.forEach((el, index) => {
@@ -77,17 +91,16 @@ async function getList(req, res, next) {
       ok: true,
       batchListLength,
       batchList,
-      hash: hashString ? hashCode(hashString) : null
+      hash: hashString ? hashCode(hashString) : null,
     };
     if (process.env.NODE_ENV === "development") {
       output.queryOptions = { match, projection, collection, limit };
-    };
+    }
 
     res.status(200).json(output);
-  }
-  catch (err) {
+  } catch (err) {
     next(err);
-  };
-};
+  }
+}
 
 export default getList;
--- server/src/routes/batch/pages/getRunning.js
+++ server/src/routes/batch/pages/getRunning.js
@@ -2,13 +2,13 @@ import Mongo from "../../../components/mongo";
 import FeConfigSingleton from "../../../components/FeConfigSingleton";
 
 async function getRunning(req, res, next) {
-
   try {
     let stationId = req.params.stationId;
     let projection = {
       _id: true,
       id: true,
       label: "$info.production_order",
+      part_id: "$info.part_id",
       start_time: true,
       end_time: true,
       station: true,
@@ -18,13 +18,17 @@ async function getRunning(req, res, next) {
       let err = new Error(`${stationId} is not a valid station id`);
       err.status = 400;
       throw err;
-    };
-    let result = await Mongo.db.collection("batch").findOne({ station: new Mongo.ObjectId(stationId), status: "running" }, { projection });
+    }
+    let result = await Mongo.db
+      .collection("batch")
+      .findOne(
+        { station: new Mongo.ObjectId(stationId), status: "running" },
+        { projection }
+      );
     res.status(200).json({ ok: true, batch: result ?? null });
-  }
-  catch (err) {
+  } catch (err) {
     next(err);
-  };
-};
+  }
+}
 
 export default getRunning;
