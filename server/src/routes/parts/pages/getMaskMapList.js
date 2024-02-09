9ef8f9455023b3d64f79d9c0e1cb8c30070f005a
Adriane Santos <adriane.santos@eyeflow.ai>
2024-02-09 11:52:49 -0300

    update

--- server/src/routes/parts/pages/getMaskMapList.js
+++ server/src/routes/parts/pages/getMaskMapList.js
@@ -7,10 +7,12 @@ async function getMaskMapList(req, res, next) {
     let maskMapId = feConfigDoc.pages["HMI"].options.maskMapId
 
     let maskMapListURL = `${feConfigDoc.pages["HMI"].options.maskMapListURL}/${maskMapId}/examples.json`
+    let maskMapInfoURL = `${feConfigDoc.pages["HMI"].options.maskMapListURL}/${maskMapId}/parms.json`
 
-    const response = await axios.get(maskMapListURL);
+    const maskList = await axios.get(maskMapListURL);
+    const maskMapInfo = await axios.get(maskMapInfoURL);
 
-    res.status(200).json({ ok: true, data: response.data });
+    res.status(200).json({ ok: true, data: { maskList: maskList.data, maskMapInfo: maskMapInfo.data }});
   }
   catch (err) {
     next(err);
