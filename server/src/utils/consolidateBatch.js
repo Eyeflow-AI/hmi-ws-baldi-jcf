d3e0876400d93733cdbde096177cb36673fbb4c0
Felipe <fas.felipesantos@gmail.com>
2024-02-26 15:09:43 -0300

    added end_time to batch

--- server/src/utils/consolidateBatch.js
+++ server/src/utils/consolidateBatch.js
@@ -1,34 +1,46 @@
 import Mongo from "../components/mongo";
 
-
-async function consolidateBatch(batchId, status="closed") {
-
+async function consolidateBatch(batchId, status = "closed") {
   if (["running", "paused", "closed"].includes(status) === false) {
     let err = new Error(`Invalid status ${status}`);
     throw err;
-  };
+  }
 
-  let batchDocument = await Mongo.db.collection("batch").findOne({ _id: batchId });
+  let batchDocument = await Mongo.db
+    .collection("batch")
+    .findOne({ _id: batchId });
   if (!batchDocument) {
     let err = new Error(`Batch with _id ${batchId} does not exist`);
     err.status = 400;
     throw err;
-  };
+  }
 
-  let lastEvent = await Mongo.db.collection("events").findOne({ "event_data.batch_id": batchId }, { sort: { event_time: -1 } });
+  let lastEvent = await Mongo.db
+    .collection("events")
+    .findOne({ "event_data.batch_id": batchId }, { sort: { event_time: -1 } });
   let updateResult;
   if (lastEvent) {
     delete lastEvent.event_data.batch_id;
-    updateResult = await Mongo.db.collection("batch").updateOne({ _id: batchId }, { $set: { batch_data: lastEvent.event_data, status } });
-  //   if (updateResult.modifiedCount !== 1) {
-  //     let err = new Error(`Batch with _id ${batchId} was not updated`);
-  //     err.status = 500;
-  //     throw err;
-  //   }
-  //   let deleteResult = await Mongo.db.collection("events").deleteMany({ "event_data.batch_id": batchId, event_time: { $lte: lastEvent.event_time } });
-  }
-  else {
-    updateResult = await Mongo.db.collection("batch").updateOne({ _id: batchId }, { $set: { status } });
+    updateResult = await Mongo.db
+      .collection("batch")
+      .updateOne(
+        { _id: batchId },
+        { $set: { batch_data: lastEvent.event_data, status } }
+      );
+    //   if (updateResult.modifiedCount !== 1) {
+    //     let err = new Error(`Batch with _id ${batchId} was not updated`);
+    //     err.status = 500;
+    //     throw err;
+    //   }
+    //   let deleteResult = await Mongo.db.collection("events").deleteMany({ "event_data.batch_id": batchId, event_time: { $lte: lastEvent.event_time } });
+  } else {
+    let update = { $set: { status } };
+    if (["paused", "closed"].includes(status)) {
+      update["$set"]["end_time"] = new Date();
+    }
+    updateResult = await Mongo.db
+      .collection("batch")
+      .updateOne({ _id: batchId }, update);
   }
 
   if (updateResult.modifiedCount !== 1) {
@@ -38,6 +50,6 @@ async function consolidateBatch(batchId, status="closed") {
   }
 
   return true;
-};
+}
 
 export default consolidateBatch;
