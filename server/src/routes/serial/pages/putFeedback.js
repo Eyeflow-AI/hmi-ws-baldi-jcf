f37c7b2b83f56007defa173d66c686794e6562c1
Felipe <fas.felipesantos@gmail.com>
2024-03-04 12:54:29 -0300

    adjust for serial

--- server/src/routes/serial/pages/putFeedback.js
+++ server/src/routes/serial/pages/putFeedback.js
@@ -1,7 +1,6 @@
 import Mongo from "../../../components/mongo";
 
 async function putFeedback(req, res, next) {
-
   try {
     let stationId = req.params.stationId;
     let serialId = req.params.serialId;
@@ -9,47 +8,61 @@ async function putFeedback(req, res, next) {
     const tokenData = { ...req?.app?.auth } ?? {};
     let query = {
       station: new Mongo.ObjectId(stationId),
-      'event_data.inspection_id': serialId,
-      'event_data.inspection_result.check_list.region.name': regionName,
+      "event_data.inspection_id": serialId,
+      "event_data.inspection_result.check_list.region.name": regionName,
     };
-    let result = await Mongo.db.collection("inspection_events").updateOne(query,
-      {
+    let result = await Mongo.db
+      .collection("inspection_events")
+      .updateOne(query, {
         $set: {
-          'event_data.inspection_result.check_list.region.$.feedback': true,
+          "event_data.inspection_result.check_list.region.$.feedback": true,
         },
-      }
+      });
 
-    );
-
-    result = await Mongo.db.collection('inspection_events').updateOne({ 'event_data.inspection_id': serialId, station: new Mongo.ObjectId(stationId) }, { $set: { feedback_time: new Date() } });
+    result = await Mongo.db
+      .collection("inspection_events")
+      .updateOne(
+        {
+          "event_data.inspection_id": serialId,
+          station: new Mongo.ObjectId(stationId),
+        },
+        { $set: { feedback_time: new Date() } }
+      );
     if (result) {
-      let document = await Mongo.db.collection("inspection_events").findOne({ 'event_data.inspection_id': serialId, station: new Mongo.ObjectId(stationId) });
-      let feedbackRegion = document.event_data.inspection_result.check_list.region.find(region => region.name === regionName);
-      let dataset_id = document.event_data.inspection_result.check_list.info.dataset_id;
+      let document = await Mongo.db
+        .collection("inspection_events")
+        .findOne({
+          "event_data.inspection_id": serialId,
+          station: new Mongo.ObjectId(stationId),
+        });
+      let feedbackRegion =
+        document.event_data.inspection_result.check_list.region.find(
+          (region) => region.name === regionName
+        );
+      let dataset_id =
+        document.event_data.inspection_result.check_list.info.dataset_id;
       await Mongo.db.collection("events_to_upload").insertOne({
         data: feedbackRegion,
         original_id: document._id,
-        original_collection: 'inspection_events',
+        original_collection: "inspection_events",
         uploaded: false,
-        image_file: feedbackRegion.image_file,
-        image_path: feedbackRegion.image_path,
-        image_scale: feedbackRegion.image_scale,
+        image_file: feedbackRegion.image.image_file,
+        image_path: feedbackRegion.image.image_path,
+        image_scale: feedbackRegion.image.image_scale,
         dataset_id,
         feedback_user: tokenData.tokenPayload.payload.username,
       });
 
       res.status(200).json({ ok: true });
-    }
-    else {
+    } else {
       let err = new Error(`Could not update document ${serialId}`);
-      console.log({ result })
+      console.log({ result });
       err.status = 400;
       throw err;
     }
-  }
-  catch (err) {
+  } catch (err) {
     next(err);
-  };
-};
+  }
+}
 
 export default putFeedback;
