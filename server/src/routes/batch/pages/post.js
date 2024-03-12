47ac4511a4b38e3bf74a1c0300429aac1151090b
Adriane Santos <adriane.santos@eyeflow.ai>
2024-03-12 11:50:11 -0300

    ..

--- server/src/routes/batch/pages/post.js
+++ server/src/routes/batch/pages/post.js
@@ -148,7 +148,7 @@ async function post(req, res, next) {
       },
       part_data: { ...partData, profile_parms: partData.color_profile },
     };
-    // delete postRequestBody.part_data.color_profile;
+    delete postRequestBody.part_data.color_profile;
 
     postRequestBody.env_var = lodash.cloneDeep(postRequestBody);
     try {
